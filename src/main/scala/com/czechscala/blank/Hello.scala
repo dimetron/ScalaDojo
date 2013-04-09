dcf57e10340c6cfac5898dba11a48575a1843ad6
Dmytro Rashko <dimetron@me.com>
2013-04-09 20:09:40 +0200

    initial commit

---
+++ src/main/scala/com/czechscala/blank/Hello.scala
@@ -0,0 +1,10 @@
+package com.czechscala.blank
+
+object Hello {
+
+  def main(args: Array[String]) {
+  	println(sayHello)
+  }
+
+  def sayHello: String = "Hello World!"
+}
---
+++ src/test/scala/com/czechscala/blank/HelloTest.scala
@@ -0,0 +1,10 @@
+package com.czechscala.blank
+
+import org.scalatest.FunSuite
+ 
+class HelloTest extends FunSuite {
+ 
+  test("world is greeted properly") {
+    assert(Hello.sayHello === "Hello World!")
+  }
+}
